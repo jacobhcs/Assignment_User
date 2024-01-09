bf13c9d0a6cd9f402da3bc6f36925c464cf1232f
jacobhcs <jaharr1999@gmail.com>
2024-01-09 03:38:24 -0500

    changes

--- User.py
+++ User.py
@@ -18,8 +18,13 @@ class User:
     self.gold_card_points -= amount
 
 user_profile = User("Jacob", "Harris", "jacobh@aol.com", 24)
+user_profile2 = User("John", "Doe", "johnd@aol.com", 99)
+user_profile3 = User("Jane", "Doe", "janed@aol.com", 99)
 print(user_profile)
-user_profile.display_info()
-user_profile.enroll()
-user_profile.spend_points(25)
+user_profile2.enroll()
+user_profile.spend_points(50)
+user_profile2.spend_points(80)
 print(user_profile.is_rewards_memeber, user_profile.gold_card_points)
+user_profile.display_info()
+user_profile2.display_info()
+user_profile3.display_info()
