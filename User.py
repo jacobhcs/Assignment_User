0e8bdcac62217131c0f2089027544d5556efc1c2
jacobhcs <jaharr1999@gmail.com>
2024-01-09 03:58:11 -0500

    chained methods

--- User.py
+++ User.py
@@ -11,20 +11,21 @@ class User:
     print(self.last_name)
     print(self.email)
     print(self.age)
+    return self
   def enroll(self):
     self.is_rewards_memeber = True
     self.gold_card_points = 200
+    return self
   def spend_points(self, amount):
     self.gold_card_points -= amount
+    return self
 
 user_profile = User("Jacob", "Harris", "jacobh@aol.com", 24)
 user_profile2 = User("John", "Doe", "johnd@aol.com", 99)
 user_profile3 = User("Jane", "Doe", "janed@aol.com", 99)
-print(user_profile)
-user_profile2.enroll()
-user_profile.spend_points(50)
-user_profile2.spend_points(80)
+
+user_profile.enroll().display_info().spend_points(50)
+user_profile2.enroll().display_info().spend_points(80)
+user_profile3.enroll().display_info().spend_points(100)
+
 print(user_profile.is_rewards_memeber, user_profile.gold_card_points)
-user_profile.display_info()
-user_profile2.display_info()
-user_profile3.display_info()
